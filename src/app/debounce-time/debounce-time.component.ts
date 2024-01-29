dd9a72ff76095ea91c812e087b82c15b10c4ca90
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2024-01-28 20:45:11 -0500

    First Last and ElementAt from RxJs Operators

--- src/app/debounce-time/debounce-time.component.ts
+++ src/app/debounce-time/debounce-time.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
-import { debounceTime, take, takeWhile } from "rxjs";
+import {
+  Observable,
+  debounceTime,
+  elementAt,
+  first,
+  from,
+  last,
+  take,
+  takeLast,
+  takeWhile,
+} from "rxjs";
 
 @Component({
   selector: "app-debounce-time",
@@ -14,14 +24,38 @@ export class DebounceTimeComponent implements OnInit {
     search: new FormControl("Start Search"),
   });
 
+  categories = ["Mobile", "Charger", "Headphones", "Mic", "TV"];
+  category$: Observable<string> = from(this.categories);
+
+  ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  rank$: Observable<number> = from(this.ranks);
+
   ngOnInit(): void {
     this.form
       .get("search")
       ?.valueChanges.pipe(takeWhile((value) => this.returnValue(value)))
-      //take(4)
+      //take(4) // It will take 4 charcters
       .pipe(debounceTime(2000))
       .subscribe((data) => {
         console.log(data);
+        this.category$
+          .pipe(
+            //takeLast(2) It will return last 2 values from array
+            first() // Its return first value of array
+          )
+          .subscribe((data1) => {
+            //console.log(`TakeLast ${data1}`);
+            console.log(`First: ${data1}`);
+          });
+        this.rank$
+          .pipe(
+            // last()  Its return last value of array
+            elementAt(4)
+          )
+          .subscribe((data2) => {
+            //console.log(`Last: ${data2}`);
+            console.log(`Element At: ${data2}`);
+          });
       });
   }
   returnValue(value: any): boolean {
